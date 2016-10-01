bb6d3cff8723e4f59ab9e6801b304b023eeca522
Matt Gaunt <matt@gauntface.co.uk>
2016-09-30 21:42:14 -0700

    Trailing out push testing service

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,3 +1,29 @@
 # Contributing to WebPush
 WebPush is an open source library.
 Feel free to contribute by submitting a pull request or creating (and solving) issues!
+
+## Running Tests
+
+First, you will need to create your own configuration file by copying
+phpunit.dist.xml to phpunit.xml and filling in the fields you need for
+testing (i.e. STANDARD_ENDPOINT, GCM_API_KEY etc.).
+
+Then, download [phpunit](https://phpunit.de/) and tests with one of the
+following commands:
+
+**For All Tests**
+    php phpunit.phar
+
+**For a Specific Test File**
+    php phpunit.phar tests/EncryptionTest.php
+
+**For a Single Test**
+    php phpunit.phar . --filter "/::testPadPayload( .*)?$/" (regex)
+
+Some tests have a custom decorator @skipIfTravis. The reason is that
+there's no way in Travis to update the push subscription, so the endpoint
+in my phpunit.travis.xml would ultimately expire
+(and require a human modification), and the corresponding tests would fail.
+But locally, these tests are handy.
+
+On Ubuntu you may need to install php-gmp, php-mbstring, php-curl.
---
+++ tests/PushServiceTest.php
@@ -0,0 +1,136 @@
+<?php
+
+/*
+ * This file is part of the WebPush library.
+ *
+ * (c) Louis Lagrange <lagrange.louis@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Minishlink\WebPush\WebPush;
+
+class PushServiceTest extends PHPUnit_Framework_TestCase
+{
+    private static $testSuiteId;
+    private static $gcmSenderId = "653317226796";
+    private static $gcmApiKey = "AIzaSyBBh4ddPa96rQQNxqiq_qQj7sq1JdsNQUQ";
+
+    /** @var WebPush WebPush with correct api keys */
+    private $webPush;
+
+    public static function setUpBeforeClass()
+    {
+        $curl = curl_init("http://localhost:8090/api/start-test-suite/");
+        curl_setopt_array($curl, array(
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true
+        ));
+        $resp = curl_exec($curl);
+        $parsedResp = json_decode($resp);
+        self::$testSuiteId = $parsedResp->{'data'}->{'testSuiteId'};
+
+        fwrite(STDOUT, "Test Suite ID: " . self::$testSuiteId . "\n");
+
+        // Close request to clear up some resources
+        curl_close($curl);
+    }
+
+    protected function setUp()
+    {
+        $this->webPush = new WebPush(array('GCM' => self::$gcmApiKey));
+        $this->webPush->setAutomaticPadding(false);
+    }
+
+    public function testChromeStable()
+    {
+        $dataString = json_encode(array(
+            "testSuiteId" => self::$testSuiteId,
+            "browserName" => "chrome",
+            "browserVersion" => "stable",
+            "gcmSenderId" => self::$gcmSenderId
+        ));
+        $curl = curl_init("http://localhost:8090/api/get-subscription/");
+        curl_setopt_array($curl, array(
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $dataString,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Content-Length: " . strlen($dataString)
+            )
+        ));
+        $resp = curl_exec($curl);
+        $parsedResp = json_decode($resp);
+
+        $testId = $parsedResp->{'data'}->{'testId'};
+        $subscription = $parsedResp->{'data'}->{'subscription'};
+        $endpoint = $subscription->{'endpoint'};
+        $keys = $subscription->{'keys'};
+        $auth = $keys->{'auth'};
+        $p256dh = $keys->{'p256dh'};
+
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        $payload = 'hello';
+        $sendResp = $this->webPush->sendNotification($endpoint, $payload, $p256dh, $auth, true);
+        $this->assertTrue($sendResp);
+
+        $dataString = json_encode(array(
+            "testSuiteId" => self::$testSuiteId,
+            "testId" => $testId
+        ));
+        $curl = curl_init("http://localhost:8090/api/get-notification-status/");
+        curl_setopt_array($curl, array(
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $dataString,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Content-Length: " . strlen($dataString)
+            )
+        ));
+        $resp = curl_exec($curl);
+        $parsedResp = json_decode($resp);
+
+        $messages = $parsedResp->{'data'}->{'messages'};
+        $this->assertEquals(count($messages), 1);
+        $this->assertEquals($messages[0], $payload);
+    }
+
+    protected function tearDown()
+    {
+        fwrite(STDOUT, "tearDown()\n");
+    }
+
+    public static function tearDownAfterClass()
+    {
+        $dataString = "{ \"testSuiteId\": " . self::$testSuiteId . " }";
+        $curl = curl_init("http://localhost:8090/api/end-test-suite/");
+        curl_setopt_array($curl, array(
+            CURLOPT_POST => 1,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POSTFIELDS => $dataString,
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Content-Length: " . strlen($dataString)
+            )
+        ));
+        $resp = curl_exec($curl);
+        $parsedResp = json_decode($resp);
+
+        fwrite(STDOUT, "Ending Test Suite ID: " . self::$testSuiteId . "\n");
+
+        self::$testSuiteId = null;
+        // Close request to clear up some resources
+        curl_close($curl);
+    }
+
+    protected function onNotSuccessfulTest($e)
+    {
+        fwrite(STDOUT, "onNotSuccessfulTest()\n");
+        throw $e;
+    }
+}
