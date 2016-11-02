b030dd78669be9202551357288521fbb2d9f57e4
Matt Gaunt <matt@gauntface.co.uk>
2016-11-01 20:45:57 -0700

    Adding Integration tests

--- tests/WebPushTest.php
+++ tests/WebPushTest.php
@@ -32,7 +32,7 @@ class WebPushTest extends PHPUnit_Framework_TestCase
             $this->markTestSkipped('This test does not run on Travis.');
         }
     }
-    
+
     public static function setUpBeforeClass()
     {
         self::$endpoints = array(
@@ -111,6 +111,9 @@ class WebPushTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($this->webPush->flush());
     }
 
+    /**
+     * @skipIfTravis
+     */
     public function testSendGCMNotificationWithoutGCMApiKey()
     {
         $webPush = new WebPush();
