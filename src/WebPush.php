57db806e41d05c10dce1f5e49c2dd8f4ee5ee416
t1gor <igor.timoshenkov@gmail.com>
2018-11-05 11:01:24 +0300

    Change flush() signature for easier mocking

--- src/WebPush.php
+++ src/WebPush.php
@@ -129,10 +129,10 @@ class WebPush
 	 *
 	 * @param null|int $batchSize Defaults the value defined in defaultOptions during instantiation (which defaults to 1000).
 	 *
-	 * @return \Generator
+	 * @return \iterable
 	 * @throws \ErrorException
 	 */
-    public function flush(?int $batchSize = null) : \Generator
+    public function flush(?int $batchSize = null) : \iterable
     {
         if (empty($this->notifications)) {
 	        yield from [];
