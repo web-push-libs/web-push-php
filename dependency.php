8aa117c64491822fc6933663a9bb722921f11e1f
Ryan <ryanccolson@gmail.com>
2019-06-26 09:06:11 -0700

    removing unnecessary dependency

--- dependency.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (ini_get('mbstring.func_overload') >= 2) {
-    throw new RuntimeException('You must disable mbstring.func_overload for str* function in order to use web-push-php. You can fix this in your php.ini.');
-}
